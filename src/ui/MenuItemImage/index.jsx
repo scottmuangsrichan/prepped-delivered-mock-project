40b4519134f8a73bdfcb992b26e549f905648a13
Scott Muangsrichan <scott@modernphase.io>
2022-12-31 08:32:24 -0800

    ...

--- src/ui/MenuItemImage/index.jsx
+++ src/ui/MenuItemImage/index.jsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import { useEffect } from "react";
+
+function getServerSideProps(){
+  const uri = "";
+}
 
 function MenuItemImage({alt , imgUrl}){
   return (
   <div className="w-[35%]">
     <Image
-      src="https://images.unsplash.com/photo-1484980972926-edee96e0960d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+      src={imgUrl}
       alt={alt}
       layout="responsive"
       width={100}
