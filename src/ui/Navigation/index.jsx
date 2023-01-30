7ee1943a0ed62b615b5eba78bfa3a50d749668b5
Scott Muangsrichan <scottmuangsrichan@Scotts-Mac-mini.local>
2023-01-29 18:50:56 -0800

    refactored navigation component

--- src/ui/Navigation/index.jsx
+++ src/ui/Navigation/index.jsx
@@ -6,6 +6,7 @@ export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
   const [width , setWidth] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
 
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
       function handleResize() {
@@ -21,24 +22,17 @@ export default function Navigation() {
       <div className='bg-white sticky top-0 z-10 w-screen'> 
       <AnnouncementBar />
         <nav className="flex flex-row justify-between py-6 px-8 items-center font-medium text-xl">
-          <div><Link href="/">Modern Phase</Link></div>
+          <NavigationLink link="/" linkName="Prepped Delivered" />
           <div>
-            {width <= 475 ? 
+            {
+            width <= 475 ? 
               <div onClick={() => setIsOpen(!isOpen)}>
                 <h1>Menu</h1>
                   { isOpen ?
                     <div>
-                      <div className='m-auto px-1'>
-                        <ul className='flex flex-col gap-6 text-base justify-center items-center'>
-                          <li><Link href="/plans-and-menu"><a className='text-sm hover:text-blue-500 font-semibold'>Plans & Menu</a></Link></li>
-                          <li><Link href="/why-mealprep"><a className="text-sm hover:text-blue-500 font-semibold">How it Works</a></Link></li>
-                          <li><Link href="/gift-cards"><a className="text-sm hover:text-blue-500 font-semibold">Gifts</a></Link></li>
-                          <li><Link href="/"><a className="text-sm hover:text-blue-500 font-semibold">Refer A Friend</a></Link></li>
-                          <li><Link href="/"><a className="text-sm hover:text-blue-500 font-semibold">Help</a></Link></li>
-                        </ul>
-                      </div>
+                      <NavigationList></NavigationList>
                       <div className='flex items-center justify-center gap-3'>
-                        <Link href="/login" className='text-sm hover:text-blue-500 font-semibold'><a>Login</a></Link>
+                        <NavigationLink link={"/login"} linkName={"Login"}/>
                         <div>
                           <button className='text-sm w-100 bg-blue-500 px-4 py-2 text-white hover:bg-blue-900 font-semibold'><Link href="/join-now">Sign Up</Link></button>
                         </div>
@@ -46,17 +40,40 @@ export default function Navigation() {
                     </div> : null 
                   }
               </div>
-              : <div className="w-screen">
-                  <ul className="flex flex-row justify-evenly">
-                    <li><Link href="/plans-and-menu"><a className='text-sm hover:text-blue-500 font-semibold'>Plans & Menu</a></Link></li>
-                    <li><Link href="/why-mealprep"><a className="text-sm hover:text-blue-500 font-semibold">How it Works</a></Link></li>
-                    <li><Link href="/gift-cards"><a className="text-sm hover:text-blue-500 font-semibold">Gifts</a></Link></li>
-                    <li><Link href="/"><a className="text-sm hover:text-blue-500 font-semibold">Refer A Friend</a></Link></li>
-                    <li><Link href="/"><a className="text-sm hover:text-blue-500 font-semibold">Help</a></Link></li>
-                  </ul>
-                </div>}
+              : <NavigationList></NavigationList>
+                }
               </div>
         </nav>
       </div>
+    )
+  }
+
+  function NavigationLink({link, linkName}){
+    return (
+      <Link  href={link}>
+        <a className="text-sm font-semibold hover:text-blue-500">{linkName}</a>
+      </Link>
+    )
+  }
+
+  function NavigationList(){
+    const navigationLinks = [
+      { name: 'Plans & Menu', href: '/plans-and-menu' },
+      { name: 'How it Works', href: '/why-mealprep' },
+      { name: 'Gifts', href: '/gift-cards' },
+      { name: 'Refer A Friend', href: '/refer-a-friend' },
+      { name: 'Help', href: '/help' },
+    ];
+
+    return (
+      <div className='m-auto px-1'>
+        <ul className="md:flex md:gap-5">
+          {navigationLinks.map((link) => (
+            <li key={link.name}>
+              <NavigationLink link={link.href} linkName={link.name}>{link.name}</NavigationLink>
+            </li>
+          ))}
+        </ul>
+    </div>
     )
   }
